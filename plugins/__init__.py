f99c7ee081246d3a424c494c42046dcebf6f095e
S9yd3R <97139901+S9yd3R@users.noreply.github.com>
2022-11-28 08:18:53 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,2 +1,11 @@
+from aiohttp import web
+from .route import routes
+
+
 __all__ = ["spotify","youtube","getsong","start"]
 
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
