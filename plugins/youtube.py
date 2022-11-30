535d569fa0b5be08f733c0eb8475087ec9e7caa3
S9yd3R <97139901+S9yd3R@users.noreply.github.com>
2022-11-30 06:18:46 +0530

    Update youtube.py

--- plugins/youtube.py
+++ plugins/youtube.py
@@ -10,8 +10,47 @@ from pyrogram.types.messages_and_media import Message
 async def yt_mp3(bot:Client,msg:Message) :
     chat_id = msg.chat.id
     if str(msg.text).split("/")[3].split("?")[0] == "playlist" :
-        await msg.reply_text("ᴛʜɪs ғᴇᴀᴛᴜʀᴇ ɪs sᴛɪʟʟ ᴜɴᴅᴇʀ ᴅᴇᴠᴇʟᴏᴘᴍᴇɴᴛ ᴘʟᴇᴀsᴇ ᴅᴏ ᴡᴀɪᴛ ғᴏʀ ᴛʜᴇ ɴᴇxᴛ ᴜᴘᴅᴀᴛᴇ ...")
-    
+        start = time.time()
+        completed = 0
+        failed = 0
+
+
+        message = await msg.reply_text(f"ᴘʟᴇᴀsᴇ ʙᴇ ᴘᴀᴛɪᴇɴᴛ ᴛʜɪs ᴡɪʟʟ ᴛᴀᴋᴇ ᴀ ᴡʜɪʟᴇ .\ncompleted : {completed}/{length}")
+
+
+        try :
+            playlist = Playlist.get(str(msg.text))
+            length = len(playlist["videos"])
+            links = []
+            for i in range(length):
+                links.append(playlist["videos"][i]["link"])
+
+            for link in links :
+                info = Video.get(link)
+                video = Video.get(link)["link"]
+                filename = f"{info['title']}.mp3"
+                options={
+                        'noplaylist':True,
+                        'format':'bestaudio/best',
+                        'keepvideo':False,
+                        'outtmpl':filename,
+                        }
+
+                with YoutubeDL(options) as ydl:
+                    ydl.download(video)
+                await msg.reply_document(filename,quote=False)
+                completed += 1
+                os.remove(filename)
+                await bot.edit_message_text(chat_id=chat_id,message_id=message.id,text=f"ᴘʟᴇᴀsᴇ ʙᴇ ᴘᴀᴛɪᴇɴᴛ ᴛʜɪs ᴡɪʟʟ ᴛᴀᴋᴇ ᴀ ᴡʜɪʟᴇ \n\ncoмpleтed : {completed}/{length}\nғᴀɪʟᴇᴅ : {failed}")
+        except :
+            failed += 1
+            await bot.edit_message_text(chat_id=chat_id,message_id=message.id,text=f"ᴘʟᴇᴀsᴇ ʙᴇ ᴘᴀᴛɪᴇɴᴛ ᴛʜɪs ᴡɪʟʟ ᴛᴀᴋᴇ ᴀ ᴡʜɪʟᴇ \n\ncoмpleтed : {completed}/{length}\nғᴀɪʟᴇᴅ : {failed}")
+        stop = time.time()
+        delta = y - x
+        delta = str(z).split(".")[0]
+        delta = int(z)
+        await bot.delete_messages(chat_id,message_ids=message.id)
+        await msg.reply_text(f"ᴛᴏᴛᴀʟ sᴏɴɢs : {length}\nsᴜᴄᴄᴇᴇᴅᴇᴅ : {completed}\nғᴀɪʟᴇᴅ : {failed}\n\nᴛɪᴍᴇ ᴛᴏᴏᴋ : {delta//60}.{delta%60} ᴍɪɴᴜᴛᴇs")
 
     else :
         video_info = YoutubeDL().extract_info(url=msg.text,download=False)
